2358a1978a1bbc8b2d988bea89028d294518011d
ShuntaYoshioka <K024G0096@g.neec.ac.jp>
2025-04-14 17:01:54 +0900

    test

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -2,11 +2,36 @@
 
 using namespace KamataEngine;
 
-void GameScene::Initialize() {
+void GameScene::Initialize() { 
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("./Resources./uvChecker.png"); 
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	//カメラの初期化
+	camera_.Initialize();
+
+	delete model_;
 }
 
 void GameScene::Update() {
 }
 
 void GameScene::Draw() {
+
+	//DirectCommonインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
+	//3Dモデル描画前処理
+	Model::PreDraw(dxCommon->GetCommandList());
+
+	//3Dモデル描画
+	model_->Draw(worldTransform_, camera_, textureHandle_);
+
+	//3Dモデル描画処理後
+	Model::PostDraw();
 }
