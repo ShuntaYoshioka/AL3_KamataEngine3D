93cddf86dad396f1331af7f9e39ffe2499ecd771
ShuntaYoshioka <K024G0096@g.neec.ac.jp>
2025-04-16 15:06:57 +0900

    player

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -3,10 +3,21 @@
 using namespace KamataEngine;
 
 void GameScene::Initialize() {
+	// ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("./Resources./uvChecker.png");
+
+	// 3Dモデルの生成
+	model_ = Model::Create();
+
+	// ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
+	// カメラの初期化
+	camera_.Initialize();
 }
 
-void GameScene::Update() {
-}
+void GameScene::Update() {}
 
-void GameScene::Draw() {
-}
+void GameScene::Draw() {}
+
+GameScene::~GameScene() { delete model_; }
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -2,13 +2,28 @@
 #include "KamataEngine.h"
 class GameScene {
 public:
-	//初期化
+	// 初期化
 	void Initialize();
 
-	//更新
+	// 更新
 	void Update();
 
-	//描画
+	// 描画
 	void Draw();
 
+	// デストラクタ
+	~GameScene();
+
+	// 3Dモデル
+	KamataEngine::Model* model_ = nullptr;
+
+	// ワールドトランスフォーム
+	KamataEngine::WorldTransform worldTransform_;
+
+	// カメラ
+	KamataEngine::Camera camera_;
+
+private:
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
 };
