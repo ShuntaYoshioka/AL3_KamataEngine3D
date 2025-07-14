15d7f32497e8a902918bc896b8a725e468bc2767
ShuntaYoshioka <K024G0096@g.neec.ac.jp>
2025-07-14 16:43:04 +0900

    完成

--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -2,6 +2,7 @@
 #include "KamataEngine.h"
 #include "Player.h"
 #include "Skydome.h"
+#include "Enemy.h"
 #include <vector>
 #include "MapChipField.h"
 #include "CameraController.h"
@@ -26,6 +27,9 @@ public:
 	KamataEngine::Model* modelSkydome_ = nullptr;
 
 	KamataEngine::Model* modelPlayer_ = nullptr;
+
+	KamataEngine::Model* modelEnemy_ = nullptr;
+	
 	// ワールドトランスフォーム
 	KamataEngine::WorldTransform worldTransform_;
 
@@ -37,6 +41,9 @@ public:
 
 	//sky
 	Skydome* skydome_ = nullptr;
+
+	//敵
+	Enemy* enemy_ = nullptr;
 	
 	//カメラコントローラ
 	CameraController* cameraController_ = nullptr;
